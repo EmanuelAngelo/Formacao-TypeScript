8d75a36c0686f59a07ad13991544207412ce2a34
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-14 15:48:36 -0300

    configurando typescript

---
+++ 1TypeScript-evoluindo seu javascript/app/models/negociacao.ts
@@ -0,0 +1,16 @@
+
+export class Negociacao {
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data() {
+        return this._data
+    }
+}
