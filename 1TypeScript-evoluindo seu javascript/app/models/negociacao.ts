a38234582fbb1cc6d476f4e1633a989ca443dc7f
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-15 14:12:55 -0300

    limpando formulario

--- 1TypeScript-evoluindo seu javascript/app/models/negociacao.ts
+++ 1TypeScript-evoluindo seu javascript/app/models/negociacao.ts
@@ -10,7 +10,19 @@ export class Negociacao {
         this._valor = valor;
     }
 
-    get data() {
+    get data(): Date {
         return this._data
+    }
+
+    get quantidade(): number {
+        return this._quantidade;
+    }
+
+    get valor(): number {
+        return this._valor
+    }
+
+    get volume(): number {
+        return this._quantidade * this._valor;
     }
 }
